8e9006961076c8491971b5f7ccbb73f52ff1a75e
Henk Keizer <henk@rsvier.nl>
2019-01-21 09:53:25 +0100

    Update and Delete implemented for Account

--- src/main/java/rsvier/controller/AccountController.java
+++ src/main/java/rsvier/controller/AccountController.java
@@ -3,8 +3,11 @@ package rsvier.controller;
 import java.util.List;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +42,22 @@ public class AccountController {
 			return true;
 		}
 		return false;
+	}
+	
+	@GetMapping("/{id}")
+	public Account getAccountById(@PathVariable long id) {
+		System.out.println("IN GETACCOUNTBYID: " + id);
+		// TODO check how to handle Optional correctly here
+		return accountRepository.findById(id).get();
+	}
+	
+	@PutMapping("/{id}")
+	public Account updateAccount(@RequestBody Account account) {
+		return accountRepository.save(account);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		accountRepository.deleteById(id);
 	}
 }
